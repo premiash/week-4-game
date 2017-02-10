f1d785dcff243f3b92e86cb8a96047e0cf96432c
premiash@gmail.com <premiash@gmail.com>
2017-02-10 18:13:03 -0500

    jquery code changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,137 @@
+var initializeGame = function()
+	{
+		var counter = 0;
+		var crystalValues = 0;
 
+		var targetScore = calculateRandomNumber(minNumber, maxNumber); //FromRange
+		var yourScore = 0;
+
+		$("#targetScore").text("Target Score : " + targetScore);
+
+	}
 
 var minNumber = 19;
 var maxNumber = 120
 
 var counter = 0;
 
-var randomNumber = randomNumberFromRange(minNumber, maxNumber);
+var crystalValues = [];
+var excludedValues = [];
 
-function randomNumberFromRange(min,max)
+var targetScore = calculateRandomNumber(minNumber, maxNumber); //FromRange
+var yourScore = 0;
+
+var gameover = false;
+
+var wins = 0;
+var losses = 0;
+
+var yourScore = initializeGame();
+
+
+$("#targetScore").text("Target Score : " + targetScore);
+
+calculateCrystalValues();
+
+$("#crystal-imageone").on('click', function(event) {
+	updateYourScore(crystalValues[0]);
+});
+
+$("#crystal-imagetwo").on('click', function(event) {
+	updateYourScore(crystalValues[1]);
+});
+
+$("#crystal-imagethree").on('click', function(event) {
+	updateYourScore(crystalValues[2]);
+});
+	
+$("#crystal-imagefour").on('click', function(event) {
+	updateYourScore(crystalValues[3]);
+});
+
+function updateYourScore(crystalScore)
 {
-    return Math.floor(Math.random()*(max-min+1)+min);
+	
+
+	yourScore += crystalScore;
+
+	$("#yourScore").text("Your Score : " + yourScore);
+
+	if(yourScore > targetScore)
+	{
+		loses++;
+
+		$("#loses").html("Loses :" + loses);
+
+		console.log(loses);
+		// alert("You lost!!");
+		gameover = true;
+
+	} else if (yourScore === targetScore)
+	{
+		wins++;
+
+		$("#wins").html("Wins :" + wins);
+		// alert("You won!!!");
+		gameover = true;
+	}
+
+	if (gameover) 
+	{
+		gameover = false;
+		yourScore = initializeGame();
+		return;
+	}
 }
 
-$("#targetScore").text("Target Score : " + randomNumber);
-console.log(randomNumber);	
+function calculateRandomNumber(minNumber, maxNumber)
+{
+    return Math.floor(Math.random()*(maxNumber-minNumber+1)+minNumber);
+}
+
+function calculateCrystalValues()
+{
+	for(var i=0; i<4; i++)
+	{
+		crystalValues.push(calculateRandomNumber2())
+	}
+	console.log(crystalValues);
+}
+
+function calculateRandomNumber2()
+{
+	var hasRandomNo = false;
+	var randomNumber;
+	// var test = 0;
+
+	do
+	{
+		randomNumber = calculateRandomNumber(1, 12);
+
+		if(isNotExcludedValues(randomNumber, excludedValues))
+		{
+			hasRandomNo = true;
+		}
+		// test++;
+		// console.log('test');
+	}
+	while (!hasRandomNo);
 	
-$(document).on("click", "img", function() 
+	excludedValues.push(randomNumber);
+	
+	return randomNumber;
+}
 
-
-
-
-/*	$("#crystal-imageOne").on("click", function() 
+function isNotExcludedValues(randomNumber, excludedValues)
+{
+	for(var i=0; i < excludedValues.length; i++)
 	{
-		var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-		// var randomCrystalNumber = [];
-
-		randomCrystalNumber = numberOptions[Math.floor(Math.random() * numberOptions.length)];
-
-		console.log ("random number is: " + randomCrystalNumber); 
-		$("#yourScore").html(randomCrystalNumber)
-		
-	});
-
-	$("#crystal-imageTwo").on("click", function() 
-	{
-		var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-		// var randomCrystalNumber = [];
-
-		randomCrystalNumber = numberOptions[Math.floor(Math.random() * numberOptions.length)];
-
-		console.log ("random number is: " + randomCrystalNumber); 
-		$("#yourScore").html(randomCrystalNumber)
-
-		
-	}); */
-
-
-
+		if(excludedValues[i] === randomNumber)
+		{
+			return false;
+		}
+	}
+	return true;
+}
 
 
